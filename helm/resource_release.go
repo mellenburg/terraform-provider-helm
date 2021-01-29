3a5c7853f30a2b0f8fceac3c3b64407def5cadf7
Michael Ellenburg <michaelandrewellenburg@gmail.com>
2021-01-28 17:59:38 -0800

    Pass `disable_openapi_validation` to update action
    Allows updating manifests that are not conformant to OpenAPI that could only be updated before

--- helm/resource_release.go
+++ helm/resource_release.go
@@ -589,6 +589,7 @@ func resourceReleaseUpdate(ctx context.Context, d *schema.ResourceData, meta int
 	client.Wait = d.Get("wait").(bool)
 	client.DryRun = false
 	client.DisableHooks = d.Get("disable_webhooks").(bool)
+	client.DisableOpenAPIValidation = d.Get("disable_openapi_validation").(bool)
 	client.Atomic = d.Get("atomic").(bool)
 	client.SkipCRDs = d.Get("skip_crds").(bool)
 	client.SubNotes = d.Get("render_subchart_notes").(bool)
